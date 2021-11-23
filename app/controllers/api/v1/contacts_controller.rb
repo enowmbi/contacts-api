10b112a17bcc7483dade24427d888ddf3be2515d
Enow Mbi <benowmbi@yahoo.com>
2021-11-23 10:20:28 +0100

    Add create endpoint

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -6,7 +6,18 @@ module Api
       end
 
       def create
+        contact = Contact.new(contact_params)
+        if contact.save!
+          render json: contact, status: :created
+        else
+          render json: contact.errors, status: :unprocessable_entity
+        end
+      end
 
+      private
+
+      def contact_params
+        params.require(:contact).permit(:name, :email)
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       root "contacts#index"
-      resources :contact
+      resources :contacts
     end
   end
 end
