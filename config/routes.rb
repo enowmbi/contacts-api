842bf4e0723f2129e052d9e1f982ec59ad0a8b48
Enow Mbi <benowmbi@yahoo.com>
2021-11-23 10:54:09 +0100

    Whitelist only index,show,create,update,destroy operations on contact resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       root "contacts#index"
-      resources :contacts
+      resources :contacts, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
